46ef69c1025ea8cd3745ea79aec786805e7123d2
Marcos Vitor Coelho Pereira <marcosvitorcoelho@gmail.com>
2023-11-13 21:48:24 -0300

    auth

--- src/main/java/br/com/doctorwho/StartUp.java
+++ src/main/java/br/com/doctorwho/StartUp.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cloudinary.Cloudinary;
--- src/main/java/br/com/doctorwho/controllers/AuthenticationController.java
+++ src/main/java/br/com/doctorwho/controllers/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import br.com.doctorwho.user.RegisterDto;
 import br.com.doctorwho.user.User;
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:3000", maxAge = 5000)
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
